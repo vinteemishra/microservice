d611aac27aa58af3e6fd4de05f304712bd8a9411
vintee mishra <v.mishra@afidigitalservices.com>
2024-09-05 13:53:04 +0530

    certificate with check for  manyata user

--- src/genCert.ts
+++ src/genCert.ts
@@ -7,39 +7,39 @@ import { missingParameter } from './utils';
 import { prettyPrintCertDate, nonEmptyString } from './helper';
 import { ICertData } from './interfaces';
 import { logger } from './logger';
-
+ 
 // Handler function for generating a CHAMPION Certificate using the parameters received in the POST request
-
+ 
 export const handleGeneration = async (ctx: Koa.Context, url: Url) => {
   // Check for missing parameters
   if (missingParameter(ctx, url)) {
     return;
   }
-
+ 
   ctx.type = 'png';
-
+ 
   ctx.body = await renderCertificate(ctx.request.body);
 };
-
+ 
 // Function to get the base image file for the certificate from the assests folder
-
+ 
 async function getBaseFile(
   country: string | undefined,
   isModuleCertificate: boolean = false
 ): Promise<string> {
   // Check if the country is not s tring or if its length is not equal to 2
-
+ 
   const defaultCert = 'New_certificate.png';
   const defaultModuleCert = 'New_certificate_module.png';
-
+ 
   const defaultRetval =
     isModuleCertificate === true ? defaultModuleCert : defaultCert;
-
+ 
   if (typeof country !== 'string' || country.trim().length !== 2) {
     logger.warn('Invalid country value, returning default');
     return defaultRetval;
   }
-
+ 
   const championCert = `New_certificate_${country.toUpperCase()}.png`;
   const moduleCert = `New_certificate_${country.toUpperCase()}${
     isModuleCertificate ? '_module' : ''
@@ -50,22 +50,22 @@ async function getBaseFile(
     championCert
   );
   const moduleCertPath = path.join(__dirname, 'assets/templates', moduleCert);
-
+ 
   if (isModuleCertificate) {
     return fs.existsSync(moduleCertPath) === true
       ? moduleCert
       : defaultModuleCert;
   }
-
+ 
   if (fs.existsSync(championCertPath)) {
     return fs.existsSync(moduleCertPath) === true ? championCert : defaultCert;
   }
-
+ 
   return defaultRetval;
 }
-
+ 
 // Function to create a certificate in a specific language
-
+ 
 async function renderCertificate(profile: ICertData) {
   let {
     jobTitle,
@@ -81,21 +81,21 @@ async function renderCertificate(profile: ICertData) {
     country,
     isManyataUser
   } = profile;
-
+ 
   const isModuleCertificate =
     certBody1 && certBody1.includes('module') ? true : false;
-
+ 
   // console.log("cert-service -> header: ", certHeader);
   // console.log("cert-service -> body: ", certBody);
   // console.log("cert-service -> body1: ", certBody1);
   // console.log("cert-service -> body2: ", certBody2);
   // console.log("cert-service -> language: ", language);
-
+ 
   // Make new local version of the certDates array
   const _certDates = certDates;
   // Get the latest 5 entries
   _certDates.slice(Math.max(_certDates.length - 5, 1));
-
+ 
   // Map though and prettyPrint the dates
   const prettyPrintedDates = _certDates.map((date) =>
     prettyPrintCertDate(date)
@@ -106,14 +106,14 @@ async function renderCertificate(profile: ICertData) {
   const date_3 = prettyPrintedDates[2];
   const date_4 = prettyPrintedDates[3];
   const date_5 = prettyPrintedDates[4];
-
+ 
   // Set the dimentions for the canvas
   const canvas = createCanvas(1920, 1357);
   const context = canvas.getContext('2d');
-
+ 
   // context.fillStyle = 'white'; //Make the background of the canvas white
   // context.fillRect(0, 0, 1920, 1357); //Make the background of the canvas white and fill the whole canvas
-
+ 
   // Due to specific landuages, there has to be set a font for that specific language in order to show the text correct on the certificate
   switch (language) {
     case 'Bangladesh - Bangla':
@@ -152,18 +152,18 @@ async function renderCertificate(profile: ICertData) {
       context.font = 'normal normal 30px NotoSans-Light';
       break;
   }
-
-  
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
   // Getting the base image for creating the certificate
   const basePng = await getBaseFile(country, isModuleCertificate);
   const p2 = path.join(__dirname, 'assets/templates', basePng);
   const img = await loadImage(p2);
   context.drawImage(img, 0, 0, 1920, 1357);
-
+ 
   const unfilledStar = path.join(
     __dirname,
     'assets',
@@ -172,7 +172,7 @@ async function renderCertificate(profile: ICertData) {
   const filledStar = path.join(__dirname, 'assets', 'stjerne_gul.png');
   const img_unfiledStar = await loadImage(unfilledStar);
   const img_filedStar = await loadImage(filledStar);
-
+ 
   // There will be a unfliied or a filled star to the side of the dates
   {
     !date_1
@@ -199,10 +199,10 @@ async function renderCertificate(profile: ICertData) {
       ? context.drawImage(img_unfiledStar, 1360, 770, 70, 70)
       : context.drawImage(img_filedStar, 1360, 770, 70, 70);
   }
-
+ 
   context.textAlign = 'center'; //Alignment - ie. left, right, center, start, end
   context.fillStyle = '#333'; //Color
-
+ 
   //First number is horizontal, and second number is vertical. Starting from the top left corner!
   context.fillText(jobTitle, 435, 485);
   context.fillText(name, 980, 485);
@@ -211,7 +211,7 @@ async function renderCertificate(profile: ICertData) {
   context.fillText(certBody, 710, 570);
   context.fillText(certBody1, 710, 610);
   context.fillText(certBody2, 710, 650);
-
+ 
   // context.font = "normal normal 25px 'Ubuntu Light'"; //The dates need to be a slight lesser size
   context.font = "normal normal 25px 'NotoSans-Light'"; //The dates need to be a slight lesser size
   {
@@ -229,7 +229,7 @@ async function renderCertificate(profile: ICertData) {
   {
     !date_5 ? null : context.fillText(date_5, 1525, 814);
   }
-
+ 
   if (nonEmptyString(uniqueId)) {
     context.save();
     context.font = "normal normal 20px 'monospace'";
@@ -239,19 +239,19 @@ async function renderCertificate(profile: ICertData) {
     context.fillText(`ID: ${uniqueId}`, 0, 0);
     context.restore();
   }
-
+ 
   // If the user is a Manyata user, load and draw the Manyata logo
   if(country==='IN'){
    if (isManyataUser) {
     const manyataLogoPath = path.join(__dirname, '..', 'assets', 'templates', 'manyata_logo.png');
-
+ 
     if (fs.existsSync(manyataLogoPath)) {
         try {
             const imgManyataLogo = await loadImage(manyataLogoPath);
             console.log(imgManyataLogo);
             // context.drawImage(imgManyataLogo, 1500, 50, 300, 100);
             context.drawImage(imgManyataLogo, 1500, 80, 100, 80);
-
+ 
             console.log("Manyata logo drawn successfully");
         } catch (error) {
             console.error("Error loading or drawing Manyata logo:", error);
@@ -263,7 +263,4 @@ async function renderCertificate(profile: ICertData) {
 }
   return canvas.toBuffer();
 }
-
-
-
-
+ 
